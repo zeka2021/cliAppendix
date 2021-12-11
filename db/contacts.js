04e439d9b2dabd40ddcd69567ea09b11306989bc
zeka2021 <gerasimovajeka@gmail.com>
2021-12-11 23:24:46 +0200

    start

---
+++ db/contacts.js
@@ -0,0 +1,21 @@
+
+const contactsPath = reguire("./contacts");
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = contactsPath;
---
+++ index.js
@@ -0,0 +1,4 @@
+// console.log('hello')
+
+const contactsPath = reguire("./contacts");
+console.log(contactsPath);
